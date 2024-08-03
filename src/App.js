8dde7e2bb96fd2e64a50cc9991623b79fb7c5bae
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-03 15:51:45 +0000

    Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+
     </div>
   );
 }
 
-export default App;
+export default NavBar;
