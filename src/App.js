729e032bcedcb8caee56dc61d6173ddbd661f6ff
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-05 17:42:44 +0000

    Start adding Router Switching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1> Home </h1>
-        <h1> Sign In </h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
       </Container>
     </div>
   );
