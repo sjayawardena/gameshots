69da77e0c24500c8722c3be1e2eae2987ce865f5
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-08-05 16:58:30 +0000

    style navbar text and icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <h1> Home </h1>
+        <h1> Sign In </h1>
+      </Container>
     </div>
   );
 }
 
-export default NavBar;
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,20 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png'
+import styles from '../styles/NavBar.module.css'
 
-
+/* Navbar and logo */
 const NavBar = () => {
-    return <Navbar expand="md" className="bg-body-tertiary">
+    return <Navbar className={styles.NavBar} expand="sm" fixed="top">
         <Container>
-            <Navbar.Brand href="#home"></Navbar.Brand>
+            <Navbar.Brand><img src={logo} alt="logo" height="45" border="1px solid"></img></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                    <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                    <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-                    <Nav.Link><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
+                    <Nav.Link className="nav-link"><i className="fas fa-home"></i>Home</Nav.Link>
+                    <Nav.Link className="nav-link"><i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
+                    <Nav.Link className="nav-link"><i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
